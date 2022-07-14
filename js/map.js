d44f887f6cc3f56e4215fba82daa9ffa8a21cf83
pitous-a <1229563042@qq.com>
2022-07-14 14:46:53 +0800

    1

--- js/map.js
+++ js/map.js
@@ -10,7 +10,8 @@ $(function () {
 function initMap() {
     // 百度地图API功能
     var map = new BMapGL.Map("map_div");    // 创建Map实例
-    map.centerAndZoom(new BMapGL.Point(116.718, 40.142), 11);  // 初始化地图,设置中心点坐标和地图级别
+    map.centerAndZoom(new BMapGL.Point(-118.2695, 33.9825), 11);  // 初始化地图,设置中心点坐标和地图级别
+    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
     //添加地图类型控件
     var size1 = new BMapGL.Size(10, 20);
     map.addControl(new BMapGL.MapTypeControl({
@@ -22,7 +23,7 @@ function initMap() {
     }));
 
     var bd = new BMapGL.Boundary();
-    bd.get('顺义区', function (rs) {
+    bd.get('Torrance', function (rs) {
         // console.log('外轮廓：', rs.boundaries[0]);
         // console.log('内镂空：', rs.boundaries[1]);
         var hole = new BMapGL.Polygon(rs.boundaries, {
@@ -33,7 +34,7 @@ function initMap() {
     });
     // 编写自定义函数,创建标注
     function addMarker(point) {
-        var marker = new BMap.Marker(point);
+        var marker = new BMapGL.Marker(point);
         map.addOverlay(marker);
     }
     // 随机向地图添加25个标注
@@ -43,10 +44,27 @@ function initMap() {
     var lngSpan = Math.abs(sw.lng - ne.lng);
     var latSpan = Math.abs(ne.lat - sw.lat);
     for (var i = 0; i < 25; i++) {
-        var point = new BMap.Point(sw.lng + lngSpan * (Math.random() * 0.7), ne.lat - latSpan * (Math.random() * 0.7));
+        var point = new BMapGL.Point(sw.lng + lngSpan * (Math.random() * 0.7), ne.lat - latSpan * (Math.random() * 0.7));
         addMarker(point);
     };
-    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
+    // hover
+    // map.addEventListener("onmousemove",function(e){
+    //     if(map_div.contains(window.event.srcElement))
+    //     {
+    //         console.log(e.point.lng + "," + e.point.lat);
+    //     }
+        
+    // });
+
+    // 点击
+    map.addEventListener("click",function(e){
+        if(map_div.contains(window.event.srcElement))
+        {
+            console.log(e.point.lng + "," + e.point.lat);
+        }
+        
+    });
     // 设备地图颜色
     // var mapStyle = {
     //     style: "midnight"
